f794f62a89feb392e3e4d9c07a026aa17805bd5e
austinjnr <austinobonyo@gmail.com>
2022-12-20 03:05:54 +0300

    adds landing page

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //create breakfast
+    const createBreakfast = (img, name, description) => {
+
+        const rootDiv = document.createElement('div')
+        rootDiv.classList.add('single-menu')
+
+    }
+
+})
