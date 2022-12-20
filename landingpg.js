023f32b93917335ae5343242ceaf5ed853da5d02
austinjnr <austinobonyo@gmail.com>
2022-12-20 06:25:58 +0300

    testing

--- landingpg.js
+++
@@ -1,8 +0,0 @@
-const signinbtn = document.querySelector('.signinbtn');
-const formbx = document.querySelector('.formbx'); 
-const body = document.querySelector('body')
-
-signinbtn.onclick = function(){
-    formbx.classList.remove('active')
-    body.classList.remove('active')
-}
