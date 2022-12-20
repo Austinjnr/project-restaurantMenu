6f026e20b63ee541521921c44d20c10113a4abb2
austinjnr <austinobonyo@gmail.com>
2022-12-20 07:38:12 +0300

    adds click eventlistener

---
+++ landingpg.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === '3' && password === '1') {
+    window.location = 'menu.html';
+  } else {
+    alert('Incorrect username or password');
+  }
+});
