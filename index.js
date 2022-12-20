505a44e640e289916d5dff9b33abc1738b31eec3
austinjnr <austinobonyo@gmail.com>
2022-12-20 03:22:50 +0300

    adds checkout page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     //create breakfast
-    const createBreakfast = (img, name, description) => {
+    const createBreakfast = (img, name, description, price) => {
 
         const rootDiv = document.createElement('div')
         rootDiv.classList.add('single-menu')
